cac2f101cbdc25dcdfa11683cab17475379eb87e
Zetixc <faqiihaarif@gmail.com>
2023-05-29 14:04:24 +0700

    Sinau Koding Front End CRUD

---
+++ crudproject/src/components/MainContent.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+    <div class="main-section">
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="title-main">User List</div>
+        <div class="create-button">
+          <button @click="goToCreateUser" class="btn-create">Add New User</button>
+        </div>
+      </div>
+      <hr />
+      <table-view />
+    </div>
+  </main>
+</template>
+
+<script>
+import TableView from "../components/Tableview.vue";
+export default (await import("vue")).defineComponent({
+  components: {
+    TableView,
+  },
+  methods: {
+    goToCreateUser() {
+      this.$router.push({ name: "create-user" });
+    },
+  },
+});
+</script>
+
+<style>
+.main-section {
+  padding: 20px 30px;
+}
+
+.title-main {
+  font-style: normal;
+  font-weight: 800;
+  font-size: 22px;
+  line-height: 27px;
+  color: black;
+}
+
+.btn-create {
+  height: 44px;
+  width: 199px;
+  background: #34c759;
+  border-radius: 4px;
+  border: none;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+  color: white;
+}
+</style>
---
+++ crudproject/src/views/CreateView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <card :title="'Creat User'" @backhome="back" />
+  </main>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+export default (await import("vue")).defineComponent({
+  components: {
+    Card,
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+  },
+});
+</script>
